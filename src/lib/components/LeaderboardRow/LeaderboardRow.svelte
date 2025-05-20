8faa1f039e7d425852fcda3d75f7b905d3756c2f
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2025-05-20 21:34:37 +0000

    Added a tooltip to the streak indicator

--- src/lib/components/LeaderboardRow/LeaderboardRow.svelte
+++ src/lib/components/LeaderboardRow/LeaderboardRow.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { LeaderboardEntry, Match } from '$lib/types/types';
   import { capitalize } from '$lib/utils/capitalize';
+  import Tooltip from '../Tooltip/Tooltip.svelte';
 
   const {
     index,
@@ -186,8 +187,9 @@
     </div>
     {#if showStreak}
       <div
-        class={`mx-4 flex items-center gap-2 rounded-md px-2 py-1 text-sm ${streak.type === 'win' ? 'bg-orange-500' : 'bg-blue-600 brightness-75'}`}
+        class={`mx-4 rounded-md px-2 py-1 text-sm ${streak.type === 'win' ? 'bg-orange-500' : 'bg-blue-600'}`}
       >
+      <Tooltip text={`On a ${streak.amount}-game ${streak.type === "win" ? "winning" : "losing"} streak`} class="flex items-center gap-2">
         {streak.amount}
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -199,6 +201,7 @@
             d="M159.3 5.4c7.8-7.3 19.9-7.2 27.7 .1c27.6 25.9 53.5 53.8 77.7 84c11-14.4 23.5-30.1 37-42.9c7.9-7.4 20.1-7.4 28 .1c34.6 33 63.9 76.6 84.5 118c20.3 40.8 33.8 82.5 33.8 111.9C448 404.2 348.2 512 224 512C98.4 512 0 404.1 0 276.5c0-38.4 17.8-85.3 45.4-131.7C73.3 97.7 112.7 48.6 159.3 5.4zM225.7 416c25.3 0 47.7-7 68.8-21c42.1-29.4 53.4-88.2 28.1-134.4c-4.5-9-16-9.6-22.5-2l-25.2 29.3c-6.6 7.6-18.5 7.4-24.7-.5c-16.5-21-46-58.5-62.8-79.8c-6.3-8-18.3-8.1-24.7-.1c-33.8 42.5-50.8 69.3-50.8 99.4C112 375.4 162.6 416 225.7 416z"
           />
         </svg>
+        </Tooltip>
       </div>
     {/if}
   </div>
